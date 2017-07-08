295b665c250a71e52ef6b90685d05e4ad66a194d
Marcin <marcin.barylski@tas-moto.org>
2017-07-08 20:43:03 +0200

    Fix to max search case

--- goldbach-reduction.py
+++ goldbach-reduction.py
@@ -49,7 +49,7 @@ max_num = 20000
 # Searching for max reduction
 #   o True  - maximum 2^n
 #   o False - minimal 2^n
-search_for_max_reduction = False
+search_for_max_reduction = True
 
 # Caching previous primality results
 #   o True  - auxilary sets of primes and composite numbers will grow
@@ -129,15 +129,16 @@ for k in range (min_num, max_num):
         (n2, q2, res) = gp.reduce_prime_for_goldbach (p2, search_for_max_reduction)
 
         if search_for_max_reduction:
-            if q1 + q2 > max_q:
+            if q1 + q2 > max_q and n1 >= 1 and n2 >= 1:
                 max_q1 = q1
                 max_q2 = q2
                 max_n1 = n1
                 max_n2 = n2
                 max_p1 = p1
                 max_p2 = p2
+                found = True
         else:
-            if (q1 >= 2) and (q2 >= 2):
+            if q1 >= 2 and q2 >= 2:
                 found = True
                 break
 
@@ -146,7 +147,6 @@ for k in range (min_num, max_num):
             results = str(num) + ",True," + str(max_p1) + "," + str(max_p2) + "," + str(max_n1) + "," + str(max_n2) + "," + str(max_q1) + "," + str(max_q2)
         else:
             results = str(num) + ",True," + str(p1) + "," + str(p2) + "," + str(n1) + "," + str(n2) + "," + str(q1) + "," + str(q2)
-
     else:
         Exception ("Could not find reduction")
 
